08c9b69ffc559c208d62b5c924d608eb5f729c01
Daniel Chance <dan.chance@outlook.com>
2023-07-18 19:42:59 +0100

    add price alert view model

---
+++ Models/PriceAlertSetupViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Owne.Models
+{
+    public class PriceAlertSetupViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        [Range(1000, 100000)]
+        public uint PriceThreshold { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string? ListingLink { get; set; }
+
+        public bool NotifyOnPriceIncrease { get; set; }
+
+        public bool NotifyOnPropertyDelist { get; set; }
+
+        [Required]
+        public string? NotificationType { get; set; }
+
+        [StringLength(10000)]
+        public string? Note { get; set; }
+    }
+}
