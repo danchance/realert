4bacdd9e9ff6389c8d029721e8ba2e30c04ce915
Daniel Chance <dan.chance@outlook.com>
2023-07-22 17:16:27 +0100

    move logic from controller to model

--- Controllers/PriceAlertNotificationController.cs
+++ Controllers/PriceAlertNotificationController.cs
@@ -32,51 +32,47 @@ namespace Realert.Controllers
             return View("Index");
         }
 
-        // POST: PriceAlertNotification/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /*
+         * POST: PriceAlertNotification/Create
+         */
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Name,Email,PhoneNumber,PriceThreshold,ListingLink,NotifyOnPriceIncrease,NotifyOnPropertyDelist,NotificationType,Note")] PriceAlertSetupViewModel priceAlert)
         {
-            Console.WriteLine(priceAlert.NotifyOnPriceIncrease);
-            Console.WriteLine(priceAlert.NotifyOnPropertyDelist);
             if (!ModelState.IsValid)
             {
-
                 return View("Index", priceAlert);
             }
-            // Determine target site using the listing link.
-            var url = new Uri(priceAlert.ListingLink!);
-            var targetSiteDictionary = new Dictionary<string, TargetSite> 
-            {
-                {"www.rightmove.co.uk", TargetSite.RIGHTMOVE},
-                {"www.purplebricks.co.uk", TargetSite.PURPLEBRICKS},
-            };
-            // Create new price alert property.
+
             // Create new price alert notification.
             var priceAlertNotification = new PriceAlertNotification
             {
                 Name = priceAlert.Name,
                 Email = priceAlert.Email,
                 PhoneNumber = priceAlert.PhoneNumber,
-                ListingLink = priceAlert.ListingLink,
-                TargetSite = targetSiteDictionary[url.Host],
                 PriceThreshold = priceAlert.PriceThreshold,
                 NotifyOnPriceIncrease = priceAlert.NotifyOnPriceIncrease,
                 NotifyOnPropertyDelist = priceAlert.NotifyOnPropertyDelist,
                 NotificationType = priceAlert.NotificationType,
                 Note = priceAlert.Note,
-                CreatedAt = DateTime.Today,
-                DeleteCode = "123",
 
             };
+
+            // Listing link has additional validation requirements, catch any exceptions
+            // and add view model error message.
+            try
+            {
+                priceAlertNotification.ListingLink = priceAlert.ListingLink;
+            } catch (Exception) 
+            {
+                ModelState.AddModelError("ListingLink", "Please enter a valid link. Supported sites are: Rightmove and Purplebricks.");
+                return View("Index", priceAlert);
+            }
+
+            // Add new notification to database.
             _context.Add(priceAlertNotification);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-           
-            //return View("Index", priceAlert);
         }
-    
     }
 }
