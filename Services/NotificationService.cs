b3dca15521309e2172083af63ca4ab08adb7a764
Daniel Chance <dan.chance@outlook.com>
2023-07-27 14:10:47 +0100

    add notification class

--- Services/NotificationService.cs
+++ Services/NotificationService.cs
@@ -2,5 +2,7 @@
 {
     public class NotificationService
     {
+
+        public NotificationService() { }
     }
 }
--- Services/PriceAlertService.cs
+++ Services/PriceAlertService.cs
@@ -18,7 +18,7 @@ namespace Realert.Services
         /*
          * Performs intial setup for adding a property price alert.
          */
-        public async Task<string> AddPriceAlert(PriceAlertNotification priceAlert)
+        public async Task AddPriceAlert(PriceAlertNotification priceAlert)
         {
             // Fetch details of the property.
             // Note: Do this before adding the alert as this will also validate if the
@@ -30,26 +30,95 @@ namespace Realert.Services
             await _context.SaveChangesAsync();
 
             // Add property to database.
-            PriceAlertProperty priceAlertProperty = new()
-            {
-                PropertyName = propertyScraper.PropertyName,
-                FirstScannedPrice = propertyScraper.PropertyPrice,
-                LastScannedPrice = propertyScraper.PropertyPrice,
-                PriceAlertNotificationId = priceAlert.Id,
-            };
-            _context.Add(priceAlertProperty);
-            await _context.SaveChangesAsync();
+            await AddProperty(propertyScraper.PropertyName!, propertyScraper.PropertyPrice, priceAlert.Id);
 
-            return "";
+            return;
         }
 
         /*
-         * Scan property for the current price, if it has changed from the last 
-         * scan, notify the user.
+         * Delete a price alert and the linked property.
          */
-        public async Task<string> ScanProperty(PriceAlertNotification priceAlert)
+        public async Task DeletePriceAlert(PriceAlertNotification priceAlert, bool isDelist = false)
         {
-            return "";
+            _context.Remove(priceAlert);
+            await _context.SaveChangesAsync();
+
+            // If the property was delisted and user has delist alerts on, send notification.
+            if (isDelist && priceAlert.NotifyOnPropertyDelist)
+            {
+                // Notify user
+            }
+        }
+
+        /*
+         * Add property for a price alert.
+         */
+        private async Task AddProperty(string name, int price, int alertId)
+        {
+            // Add property to database.
+            PriceAlertProperty priceAlertProperty = new()
+            {
+                PropertyName = name,
+                FirstScannedPrice = price,
+                LastScannedPrice = price,
+                PriceAlertNotificationId = alertId,
+            };
+            _context.Add(priceAlertProperty);
+            await _context.SaveChangesAsync();
+        }
+
+        /*
+         * Scan property to get its current price, if the price has changed from the 
+         * last scan, notify the user.
+         */
+        public async Task ScanProperty(PriceAlertNotification priceAlert)
+        {
+            PropertyListingWebScraper propertyScraper;
+            try
+            {
+                // Fetch current price data for the property.
+                propertyScraper = await PropertyListingWebScraper.InitializeAsync(priceAlert.ListingLink!, priceAlert.TargetSite);
+            }
+            catch (Exception)
+            {
+                // Property does not exist, delete the nofication.
+                await DeletePriceAlert(priceAlert, true);
+                return;
+            }
+
+            // Property should not be null at this point, but if it is add it and exit.
+            if (priceAlert.Property == null)
+            {
+                await AddProperty(propertyScraper.PropertyName!, propertyScraper.PropertyPrice, priceAlert.Id);
+                return;
+            }
+
+            // Property is not null, compare current price to the last scan price.
+            PriceAlertProperty property = priceAlert.Property;
+            int priceDifference = propertyScraper.PropertyPrice - property.LastScannedPrice;
+
+            // If no change in price or the price change is less than the alert threshold, exit.
+            if (priceDifference == 0 || Math.Abs(priceDifference) < priceAlert.PriceThreshold)
+            {
+                return;
+            }
+
+            // If price has decreased, send notifcation.
+            if (priceDifference < 0)
+            {
+                // Send notification
+            }
+
+            // Price increase, check if user wants to receive price increase alerts.
+            if (priceAlert.NotifyOnPriceIncrease)
+            {
+
+            }
+
+            // Update property with the latest price data.
+            property.LastScannedPrice = propertyScraper.PropertyPrice;
+            _context.Update(property);
+            await _context.SaveChangesAsync();
         }
 
     }
