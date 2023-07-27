57e4578983a1ea0ea25099b0b3977cf7f11c4ceb
Daniel Chance <dan.chance@outlook.com>
2023-07-27 10:29:29 +0100

    add logic to add a new price alert

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Realert.Controllers
             string url = "https://www.purplebricks.co.uk/Api/Propertylisting/1460288";
             string url2 = "https://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=OUTCODE%5E2593&insId=1&radius=0.0&minPrice=150000&maxPrice=200000&minBedrooms=&maxBedrooms=&displayPropertyType=houses&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false";
             string url3 = "https://www.purplebricks.co.uk/search/property-for-sale/telford?page=1&sortBy=2&location=tf2&searchRadius=10&searchType=ForSale&soldOrLet=false&latitude=52.7102563&longitude=-2.419879&betasearch=true";
-            try
+            /*try
             {
                 PropertyListingWebScraper propertyScraper = await PropertyListingWebScraper.InitializeAsync(url, TargetSite.Purplebricks);
                 Console.WriteLine(propertyScraper.PropertyName);
@@ -24,7 +24,7 @@ namespace Realert.Controllers
             } catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-            }
+            }*/
             return View();
         }
 
---
+++ Services/PriceAlertService.cs
@@ -0,0 +1,56 @@
+﻿using Realert.Data;
+using Realert.Models;
+using Realert.Scrapers;
+
+namespace Realert.Services
+{
+    public sealed class PriceAlertService
+    {
+        private readonly RealertContext _context;
+        //private readonly ILogger<PriceAlertService> _logger;
+
+        public PriceAlertService(RealertContext context)
+        {
+            _context = context;
+            //_logger = logger;
+        }
+
+        /*
+         * Performs intial setup for adding a property price alert.
+         */
+        public async Task<string> AddPriceAlert(PriceAlertNotification priceAlert)
+        {
+            // Fetch details of the property.
+            // Note: Do this before adding the alert as this will also validate if the
+            // property exists/url is valid.
+            PropertyListingWebScraper propertyScraper = await PropertyListingWebScraper.InitializeAsync(priceAlert.ListingLink!, priceAlert.TargetSite);           
+
+            // Add price alert to database.
+            _context.Add(priceAlert);
+            await _context.SaveChangesAsync();
+
+            // Add property to database.
+            PriceAlertProperty priceAlertProperty = new()
+            {
+                PropertyName = propertyScraper.PropertyName,
+                FirstScannedPrice = propertyScraper.PropertyPrice,
+                LastScannedPrice = propertyScraper.PropertyPrice,
+                PriceAlertNotificationId = priceAlert.Id,
+            };
+            _context.Add(priceAlertProperty);
+            await _context.SaveChangesAsync();
+
+            return "";
+        }
+
+        /*
+         * Scan property for the current price, if it has changed from the last 
+         * scan, notify the user.
+         */
+        public async Task<string> ScanProperty(PriceAlertNotification priceAlert)
+        {
+            return "";
+        }
+
+    }
+}
