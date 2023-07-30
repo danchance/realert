8260209ecb631df29a9e425f142d088aefb00b79
Daniel Chance <dan.chance@outlook.com>
2023-07-30 11:06:15 +0100

    add interface for alerts

---
+++ Interfaces/IAlertService.cs
@@ -0,0 +1,9 @@
+﻿namespace Realert.Interfaces
+{
+    public interface IAlertService<T>
+    {
+        Task AddAlertAsync(T alert);
+        Task DeleteAlertAsync(T alert, bool isDelist);
+        Task PerformScanAsync();
+    }
+}
